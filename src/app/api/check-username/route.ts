fcac08f1b3b55f6d7d79cac1f317104d35be52e0
Swapnashree Sahoo <swapnashree2020@gmail.com>
2025-07-19 04:04:56 +0530

    added toggle message feature and ai message

--- src/app/api/check-username/route.ts
+++ src/app/api/check-username/route.ts
@@ -1,13 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
-import User from "@/model/User";
+import UserModel from "@/model/User";
+import { z } from 'zod';
+import { usernameValidation } from "@/validation/username";
 
-export async function POST(req: NextRequest) {
-    const { username } = await req.json();
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+});
+
+export async function GET(request: NextRequest) {
     await dbConnect();
-    const user = await User.findOne({ username });
-    if (user) {
-        return NextResponse.json({ available: false, message: "Username is already taken" });
+    try {
+        const { searchParams } = new URL(request.url);
+        const queryParam = {
+            username: searchParams.get('username') || ''
+        };
+        // validate the accessed username
+        const result = UsernameQuerySchema.safeParse(queryParam);
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || [];
+            return NextResponse.json({
+                success: false,
+                message: usernameErrors.length > 0 ? usernameErrors.join(', ') : 'Invalid query parameters',
+            }, { status: 400 });
+        }
+        const { username } = result.data;
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true });
+        if (existingVerifiedUser) {
+            return NextResponse.json({
+                success: false,
+                message: 'Username is already taken'
+            }, { status: 400 });
+        }
+        return NextResponse.json({
+            success: true,
+            message: 'Username is available'
+        });
+    } catch (error) {
+        console.error("Error checking username", error);
+        return NextResponse.json({
+            success: false,
+            message: "Error checking username"
+        }, { status: 500 });
     }
-    return NextResponse.json({ available: true, message: "Username is available" });
 } 
